77853144a70fc6ac2fd7dd7dd9d2627a1b5f2645
Pedro Henrique rodrigues dos santos <pedro.rodrigues0760@gmail.com>
2026-04-09 12:53:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("MM - Madeira molhada")
+alert("Exemplo IF")
+var numero = parseFloat(prompt("Digite um valor :"))
+var numero1 = parseFloat(prompt("Digite um valor :"))
+var resultado = numero + numero1
+alert("O valor do resultado é : " + resultado)
+if (resultado > 10) {
+    alert("É maior que 10")
+}else if (resultado == 10){
+    alert("É igual a 10")
+}
+else {
+    alert("É menor que 10")
+}    
---
+++ script01.js
@@ -0,0 +1,11 @@
+alert("Jailson obama")
+var numero = parseInt(prompt("Digite um número inteiro"))
+var numero1 = parseInt(prompt("Digite outro número inteiro"))
+
+if (numero > numero1){
+    alert("A diferença entre os números é :" +  (numero - numero1) )
+}
+else 
+    {
+    alert("A diferença entre os números é :" +  (numero1 - numero) )
+}
